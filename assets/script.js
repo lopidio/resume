a5db6b442bba308da4efdd8387723d7d7c2e57d0
Guilherme Moraes <gmoraes@thoughtworks.com>
2019-01-15 16:42:04 -0200

    Adding subtitle

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,5 @@ let titles = ["Lopídio", "Virgs", "Guilherme Moraes"];
 
 function changeTitle() {
     titles = titles.concat(titles.splice(0, 1));
-    $("#title").text(titles[0]);
+    $(".g-main-title").text(titles[0]);
 }
