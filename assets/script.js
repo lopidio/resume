3089d18b2344a3e3f9cf4fe93392fbd88c88462d
Guilherme Moraes <gmoraes@thoughtworks.com>
2019-01-15 15:21:28 -0200

    Adding icons and links

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 let titles = ["Lopídio", "Virgs", "Guilherme Moraes"];
 
-    console.log('irgsdasdads')
 function changeTitle() {
     titles = titles.concat(titles.splice(0, 1));
-    console.log('irgs')
     $("#title").text(titles[0]);
 }
