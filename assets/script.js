29a9fea19ada0e2a3bf85d8efd8a5702d526ee21
Guilherme Moraes <gmoraes@thoughtworks.com>
2019-01-16 16:21:42 -0200

    Adding self-logo

--- assets/script.js
+++ assets/script.js
@@ -7,17 +7,19 @@ function changeTitle() {
 
 const headerHeight = $('.g-header').css('height').replace(/[^-\d\.]/g, '') / 2;
 window.onscroll = function () {
-    if (document.documentElement.scrollTop - headerHeight > 50 ) {
+    if (document.documentElement.scrollTop > headerHeight) {
         $('.g-header').addClass('shrink');
         $('.g-links').addClass('shrink');
         $('.g-title').addClass('shrink');
         $('.g-subtitle').addClass('shrink');
         $('.g-main-title').addClass('shrink');
+        $('.g-gui-logo').addClass('shrink');
     } else if (document.documentElement.scrollTop === 0 ){
         $('.g-header').removeClass('shrink');
         $('.g-links').removeClass('shrink');
         $('.g-title').removeClass('shrink');
         $('.g-subtitle').removeClass('shrink');
         $('.g-main-title').removeClass('shrink');
+        $('.g-gui-logo').removeClass('shrink');
     }
 };
