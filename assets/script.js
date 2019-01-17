b2dcda9933266cc7c27f6647edd8408877ebdbdf
Guilherme Moraes <gmoraes@thoughtworks.com>
2019-01-16 23:28:44 -0200

    Adding a header-body divider

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,15 @@ function changeTitle() {
 
 const headerHeight = $('.g-header').css('height').replace(/[^-\d\.]/g, '') / 2;
 window.onscroll = function () {
-    if (document.documentElement.scrollTop > headerHeight / 10) {
+    const scrollTop = document.documentElement.scrollTop;
+    if (scrollTop > 1) {
         $('.g-header').addClass('shrink');
         $('.g-links').addClass('shrink');
         $('.g-title').addClass('shrink');
         $('.g-subtitle').addClass('shrink');
         $('.g-main-title').addClass('shrink');
         $('.g-gui-logo').addClass('shrink');
-    } else if (document.documentElement.scrollTop === 0 ){
+    } else if (scrollTop === 0){
         $('.g-header').removeClass('shrink');
         $('.g-links').removeClass('shrink');
         $('.g-title').removeClass('shrink');
